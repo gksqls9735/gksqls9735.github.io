6a19763ff492d084984bd6b40d76c2a7fa7dc2a9
hb <gksqls9735@gmail.com>
2025-05-29 19:45:34 +0900

    Updates

--- vite.config.ts
+++
@@ -1,8 +0,0 @@
-// vite.config.js
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-export default defineConfig({
-  base: '/', // 루트 도메인 배포용
-  plugins: [react()],
-})
